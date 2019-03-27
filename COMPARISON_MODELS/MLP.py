afbfcd3b41ed0408dab6aed05036b5b86e4d65fe
Sayan Saha <sahasayan98@gmail.com>
2019-03-27 10:00:20 +0530

    Add files via upload

---
+++ COMPARISON_MODELS/MLP.py
@@ -0,0 +1,34 @@
+import numpy as np
+import sklearn.neural_network
+import pandas
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+xs =pandas.read_csv("data.csv", usecols =["field_soil_temp_c","field_air_temp_c","field_rh"])
+ys = pandas.read_csv("data.csv", usecols =["field_soil_wc"])
+
+
+model=sklearn.neural_network.MLPRegressor(
+    activation='logistic',
+    learning_rate_init=0.001,
+    solver='sgd',
+    learning_rate='invscaling',
+    hidden_layer_sizes=(200,),
+    verbose=True,
+    max_iter=2000,
+    tol=1e-6
+)
+
+model.fit(xs,ys)
+print('Accuracy training : {:.3f}'.format(model.score(xs, ys)))
+print('\nprediction:')
+predicted_scale=model.predict(xs)
+print(predicted_scale)
+print("RMSE", mean_squared_error(ys,predicted_scale))
+
+plt.xlabel('target')
+plt.ylabel('output')
+#plt.ylim((0,1))
+plt.scatter(ys,predicted_scale)
+plt.plot(ys,ys)
+plt.show()
